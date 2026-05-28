ee9142445cd7dc8aac840e70154f46a0265df40c
himanshuyadav2025abes <himanshuyadav59988a@gmail.com>
2026-05-28 15:04:13 +0530

    Class work

---
+++ Javascript/DemoJava.java
@@ -0,0 +1,9 @@
+public class DemoJava {
+    public static void main(String[] args) {
+        String name = "himanshu yadav";
+        int age = 22;
+        System.out.println(name + " is " + age + " years old.");
+        
+        System.out.println("Hello, World!");
+    }
+}
---
+++ vite-project/src/App.jsx
@@ -0,0 +1,16 @@
+import './App.css'
+import StudentCard from './StudentCard'
+
+function App() {
+  return (
+    <>
+      <StudentCard
+        name="Himanshu"
+        course="CSE"
+        marks="95"
+      />
+    </>
+  )
+}
+
+export default App
